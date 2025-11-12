a8ed63c30498226a8fa1a3bfdd0e14df0d82521c
Tasdev <tasniaahmed543@gmail.com>
2025-11-11 22:36:39 -0500

    Slash remove 2

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -11,7 +11,7 @@ const COLORS = {
   Neutral: "#FAAD14",
   Negative: "#FF4D4F",
 }
-const API_BASE = "https://sentiment-backend-g4ip.onrender.com/"; // Fixed: previously it was `const = ...` typo
+const API_BASE = "https://sentiment-backend-g4ip.onrender.com"; // Fixed: previously it was `const = ...` typo
 
 const InputForm = () => {
   const [text, setText] = useState("");
